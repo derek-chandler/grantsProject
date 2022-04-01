fccf09d1874b8e42e8023812ab6b390e2c93f516
21zz <zanmanfan11@gmail.com>
2022-04-01 17:29:40 -0400

    cleans cache+temp when downloading new xml

--- GrantDownloader.py
+++ GrantDownloader.py
@@ -1,5 +1,6 @@
 # required libraries
 import os
+import sys
 import zipfile
 import wget
 import requests
@@ -20,6 +21,60 @@ FULL URL EXAMPLE
 https://www.grants.gov/extract/GrantsDBExtract20220203v2.zip
 """
 
+# "current working directory"
+# aka where the python script is running in
+cwd = os.getcwd()
+
+
+# cleans temporary (*.tmp) files in case of program halt or error
+def cleanTmp():
+    try:
+        for filename in os.listdir(cwd):
+            f = os.path.join(cwd, filename)
+            if os.path.isfile(f):
+                if f.endswith(".tmp"):
+                    os.remove(f)
+    except Exception as e:
+        print("There was an exception while cleaning temp files in " + cwd)
+        print(e.with_traceback())
+
+
+# cleans old zip and XML files in cache if there is a new one
+# takes the input of the current grant file name. it should be formatted like this:
+#   GrantsDBExtract20220203
+# as is with the rest of the script.
+def cleanOldCache(currentfilename):
+    # cache directory
+    cachedir = cwd + "/cache"
+    # cache/extracted directory
+    xmldir = cachedir + "/extracted"
+    # full filepath for the current cached .zip file
+    currentzip = cachedir + "/" + currentfilename + "v2.zip"
+    # full filepath for the current cached .xml file
+    currentxml = xmldir + "/" + currentfilename + "v2.xml"
+    try:
+        for filename in os.listdir(cachedir):
+            f = os.path.join(cachedir, filename)
+            if os.path.isfile(f):
+                # if the .zip is the latest one dont remove
+                if f == currentzip:
+                    continue
+                # otherwise remove if .zip
+                else:
+                    os.remove(f)
+        for filename in os.listdir(xmldir):
+            f = os.path.join(xmldir, filename)
+            if os.path.isfile(f):
+                # if the .xml is the latest one dont remove
+                if f == currentxml:
+                    continue
+                # otherwise remove
+                else:
+                    os.remove(f)
+    except Exception as e:
+        print("There was an exception while cleaning old cache files in " + cachedir)
+        print(e.with_traceback())
+
 
 # unzips the file into cache/extracted
 # used in multiple places, so it's implemented as a function to save time
@@ -35,9 +90,6 @@ def get(_time=False):
     #################################################
     ## Cache directory creation/existence checking ##
     #################################################
-    # "current working directory"
-    # aka where the python script is running in
-    cwd = os.getcwd()
     # if cache folder does not exist, make it
     if not os.path.isdir(cwd + "/cache"):
         print("creating cache directory")
@@ -78,7 +130,7 @@ def get(_time=False):
     # remove v2.zip from the end because i cba to change later code
     filename = split_url[len(split_url)-1].split("v2.zip")[0]
 
-    print("today's file grabbed ({0}), downloading...".format(filename))
+    print("today's file grabbed ({0}), checking...".format(filename), end="")
 
     ######################################################
     ## Check if XML or zip files already exist in cache ##
@@ -91,12 +143,13 @@ def get(_time=False):
         return cwd + "/cache/extracted/" + filename + "v2.xml"
     # test if zip file exists
     if os.path.isfile(cwd + "/cache/" + filename + "v2.zip"):
-        print("zip file exists, unzipping")
+        print("zip file exists, unzipping", end="")
         unzip_xml(cwd + "/cache/" + filename + "v2.zip")
+        print("done")
         if _time:
             print("took {0}s".format(time() - initial_time))
         return cwd + "/cache/extracted/" + filename + "v2.xml"
-    print("does not exist, downloading")
+    print("does not exist\ndownloading...")
 
     ########################################################
     ## Download zip file, if necessary according to above ##
@@ -105,22 +158,31 @@ def get(_time=False):
     successful = False
     while not successful:
         try:
+            # clean up old zip files
+            cleanOldCache(filename)
             wget.download(grant_url, "cache/")
             successful = True
         # sometimes the site prevents connection due to crawl-delay
-        except ConnectionError as e:
+        except ConnectionError:
             print("connection aborted, waiting 15 seconds...")
             sleep(15)
         # ... and sometimes the site disconnects before wget wants it to...
         # the file should still be downloaded just fine
         # (manually checked sha1 checksum vs. normally downloaded file and it checked out)
-        except RemoteDisconnected as e:
+        except RemoteDisconnected:
             print("remote disconnected, verifying if file exists...")
+            # if the zip file exists, then wget downloaded it
+            # otherwise it stays as a .tmp file
             if os.path.isfile(cwd + "/cache/" + filename + "v2.zip"):
                 successful = True
                 print("file exists, continuing")
             else:
+                cleanTmp()
                 print("file does not exist, attempting re-download")
+        # clean up temporary files if the script is manually interrupted
+        except KeyboardInterrupt:
+            cleanTmp()
+            sys.exit(0)
 
     ########################################
     ## Unzip and return the FULL filepath ##
@@ -130,8 +192,3 @@ def get(_time=False):
     if _time:
         print("took {0}s".format(time() - initial_time))
     return cwd + "/cache/extracted/" + filename + "v2.xml"
-
-
-# this is for debugging. remove this line and call get() function directly from
-#   GrantsParserXML.py
-# print(get())
