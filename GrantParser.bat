364e1fb2998287f2c9b6b6c2cad538e22fdbc03f
Em3raud3 <animekamisama@gmail.com>
2022-04-22 15:29:34 -0400

    changed "python3" to "python"

--- GrantParser.bat
+++ GrantParser.bat
@@ -1,6 +1,6 @@
 @echo off
 pip install virtualenv
-python3 -m venv venv
+python -m venv venv
 call venv\Scripts\activate
 pip install -r requirements.txt
-python3 GrantsParserXML.py
+python GrantsParserXML.py
